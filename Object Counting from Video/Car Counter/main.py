eb84a56eafafaf8af0d2f75b46c974ea35f9dcf0
naufalkusumajaya <naufalkusumajaya@gmail.com>
2023-08-16 21:38:53 +0700

    Update main.py

--- Object Counting from Video/Car Counter/main.py
+++ Object Counting from Video/Car Counter/main.py
@@ -5,86 +5,94 @@ import cvzone
 import math
 from sort import *
 
-cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Video
+# Initialize video capture
+video_path = "../Videos/cars.mp4"
+cap = cv2.VideoCapture(video_path)
 
-model = YOLO("../Yolo-Weights/yolov8l.pt")
+# Initialize YOLO model
+model_path = "../Yolo-Weights/yolov8l.pt"
+model = YOLO(model_path)
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
+# List of object class names
+class_names = ["car", "truck", "bus", "motorbike"]
 
+# Load mask image
 mask = cv2.imread("mask.png")
 
-# Tracking
+# Initialize object tracker
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
-limits = [400, 297, 673, 297]
-totalCount = []
+# Define threshold limits
+threshold_limits = [400, 297, 673, 297]
+
+# Initialize tracked object IDs
+tracked_ids = []
 
 while True:
-    success, img = cap.read()
-    imgRegion = cv2.bitwise_and(img, mask)
+    # Read a frame from the video
+    success, frame = cap.read()
 
-    imgGraphics = cv2.imread("graphics.png", cv2.IMREAD_UNCHANGED)
-    img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
-    results = model(imgRegion, stream=True)
+    # Apply mask to the frame
+    masked_frame = cv2.bitwise_and(frame, mask)
 
+    # Overlay graphics on the frame
+    graphics = cv2.imread("graphics.png", cv2.IMREAD_UNCHANGED)
+    frame = cvzone.overlayPNG(frame, graphics, (0, 0))
+
+    # Detect objects using YOLO
     detections = np.empty((0, 5))
+    results = model(masked_frame, stream=True)
 
     for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            # Bounding Box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+        for box in r.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
             w, h = x2 - x1, y2 - y1
-
-            # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
-            # Class Name
             cls = int(box.cls[0])
-            currentClass = classNames[cls]
+            current_class = class_names[cls]
 
-            if currentClass == "car" or currentClass == "truck" or currentClass == "bus" \
-                    or currentClass == "motorbike" and conf > 0.3:
-                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)),
-                #                    scale=0.6, thickness=1, offset=3)
-                # cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
-                currentArray = np.array([x1, y1, x2, y2, conf])
-                detections = np.vstack((detections, currentArray))
+            if current_class in class_names and conf > 0.3:
+                current_array = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, current_array))
 
-    resultsTracker = tracker.update(detections)
+    # Update object tracking
+    tracked_objects = tracker.update(detections)
 
-    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
-    for result in resultsTracker:
-        x1, y1, x2, y2, id = result
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        print(result)
+    # Draw threshold line
+    cv2.line(frame, (threshold_limits[0], threshold_limits[1]),
+             (threshold_limits[2], threshold_limits[3]), (0, 0, 255), 5)
+
+    for obj in tracked_objects:
+        x1, y1, x2, y2, obj_id = map(int, obj)
         w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
-        cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)),
+
+        # Draw rectangle and text for each tracked object
+        cvzone.cornerRect(frame, (x1, y1, w, h), l=9,
+                          rt=2, colorR=(255, 0, 255))
+        cvzone.putTextRect(frame, f'ID: {int(obj_id)}', (max(0, x1), max(35, y1)),
                            scale=2, thickness=3, offset=10)
 
         cx, cy = x1 + w // 2, y1 + h // 2
-        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(frame, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-        if limits[0] < cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
-            if totalCount.count(id) == 0:
-                totalCount.append(id)
-                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+        # Check if object crossed the threshold
+        if threshold_limits[0] < cx < threshold_limits[2] and threshold_limits[1] - 15 < cy < threshold_limits[1] + 15:
+            if obj_id not in tracked_ids:
+                tracked_ids.append(obj_id)
+                cv2.line(frame, (threshold_limits[0], threshold_limits[1]), (
+                    threshold_limits[2], threshold_limits[3]), (0, 255, 0), 5)
 
-    # cvzone.putTextRect(img, f' Count: {len(totalCount)}', (50, 50))
-    cv2.putText(img,str(len(totalCount)),(255,100),cv2.FONT_HERSHEY_PLAIN,5,(50,50,255),8)
+    # Display total count
+    cv2.putText(frame, str(len(tracked_ids)), (255, 100),
+                cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
 
-    cv2.imshow("Image", img)
-    # cv2.imshow("ImageRegion", imgRegion)
-    cv2.waitKey(1)
+    # Display the annotated frame
+    cv2.imshow("Object Detection and Tracking", frame)
+
+    # Exit loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close windows
+cap.release()
+cv2.destroyAllWindows()
