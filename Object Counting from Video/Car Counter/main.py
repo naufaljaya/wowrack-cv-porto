e1a2ded7fa3c5b2efd949caa4884ae5e4c48f1e2
naufalkusumajaya <naufalkusumajaya@gmail.com>
2023-09-05 15:47:19 +0700

    Update main.py

--- Object Counting from Video/Car Counter/main.py
+++ Object Counting from Video/Car Counter/main.py
@@ -14,7 +14,18 @@ model_path = "../Yolo-Weights/yolov8l.pt"
 model = YOLO(model_path)
 
 # List of object class names
-class_names = ["car", "truck", "bus", "motorbike"]
+
+class_names = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 # Load mask image
 mask = cv2.imread("mask.png")
